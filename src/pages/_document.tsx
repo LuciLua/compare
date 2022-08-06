26032ecbb0a4acdee249d93fd78255e5ac127ee8
LuciLua <luci.lua81@gmail.com>
2022-08-06 01:19:00 -0300

    :star2: document page add

---
+++ src/pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Head, Html, Main, NextScript } from "next/document"
+
+function Document() {
+    return (
+        <Html>
+            <Head>
+                <title>GitHub Followers</title>
+            </Head>
+            <Main />
+            <NextScript />
+        </Html>
+    )
+}
+
+
+export default Document
